dabe8d7f661ff0f20718d7a6d65fed17967e27bb
mgreens19 <mgreenspoon19@gmail.com>
2025-02-04 13:36:03 -0500

    Another update

--- BlackWidow.py
+++ BlackWidow.py
@@ -18,4 +18,4 @@ while x != 20:
     if  ellipsis == 4:
         ellipsis = 0
     if x == 20:
-        print("\nOperating System Booted Up - Retina Scanned - Access Granted")
+        print("\n\nOperating System Booted Up - Retina Scanned - Access Granted\n")
