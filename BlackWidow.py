cfd786bcbacf944c0aa33f6deb87bcc0463cb6d1
Greenspoon <mgreenspoon19@gmail.com>
2025-02-11 13:45:42 -0500

    Added Sleep and Engaged VRS System- Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,6 +1,6 @@
 print("\n******************************************\n")
 
-print("Weather Branch - Developer: Maddox Greenspoon\n")
+print("Weather Branch - Developer: Maddox Greenspoon")
 
 # Import necessary libraries for random weather generation and timing
 import random
@@ -22,20 +22,31 @@ def vehicleResponseSystem():
     if weatherAlert == "snowing":
         print("\nThe National Weather Service has updated our alarm by 30 minutes because"
               " it is", weatherAlert, "outside.")
+        sleep(1)
+        print("VRS has been engaged only allowing us to drive 55MPH.")
     elif weatherAlert == "blizzard":
         print("\nThe National Weather Service has updated our alarm by 60 minutes because"
               " it is", weatherAlert, "outside.")
+        sleep(1)
+        print("VRS has been engaged only allowing us to drive 45MPH.")
     elif weatherAlert == "icy":
         print("\nThe National Weather Service has updated our alarm by 90 minutes because"
               " it is", weatherAlert, "outside.")
+        sleep(1)
+        print("VRS has been engaged only allowing us to drive 35MPH.")
     elif weatherAlert == "rainy":
         print("\nThe National Weather Service has updated our alarm by 10 minutes because"
               " it is", weatherAlert, "outside.")
+        sleep(1)
+        print("VRS has been engaged only allowing us to drive 65MPH.")
     elif weatherAlert == "windy":
         print("\nThe National Weather Service has updated our alarm by 5 minutes because"
               " it is", weatherAlert, "outside.")
+        sleep(1)
+        print("VRS has been engaged only allowing us to drive 70MPH.")
     else:
         print("\nThe National Weather Service is calling for", weatherAlert, "skys outside, drive safe!")
-
+        sleep(1)
+        print("VRS has been disengaged, drive safe.")
 # Execute the vehicle response system based on the weather condition
 vehicleResponseSystem()
