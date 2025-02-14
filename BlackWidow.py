169aa63a6b662bf54e16bac7ae3ed0e0a526621d
Greenspoon <mgreenspoon19@gmail.com>
2025-02-14 14:06:19 -0500

    Added gasStations Function - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -8,4 +8,9 @@ def gasLevelGauge():
   gasLevelList = ["Empty", "Low", "Quarter Tank", "Half Tank", "Three Quarter Tank", "Full Tank"]
   return random.choice(gasLevelList)
 
+def gasStations():
+    gasStationsList = ["Shell", "Marathon", "Speedway", "Circle K", "Wesco", "Meijer", "Bucees"]
+    return random.choice(gasStationsList)
+
 print(gasLevelGauge())
+print(gasStations())
